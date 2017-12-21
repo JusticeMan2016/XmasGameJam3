6b80919eac8578189b8994f924f38cd89657a5e5
Arjan <arjan.grinwis@hva.nl>
2017-12-21 14:15:15 +0100

    added tree falling functionality

---
+++ Assets/Scripts/TreeFallingBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFallingBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
