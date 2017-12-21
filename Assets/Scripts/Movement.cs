2916b4ea26d3657827b24d68cc0a2ff7ca40c9b3
Arjan <arjan.grinwis@hva.nl>
2017-12-21 12:19:38 +0100

    Added woodcutter with animation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Animation anim;
+    private Animator anim;
     // Use this for initialization
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKey("space"))
         {
-            Debug.Log("SPACE!!!!!");
+            anim.Play("Cutting");
         }
-
+           // anim.enabled = false;
     }
 }
