56e2795a3fceecf54ea49a76eabc559fe647cc5e
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2017-12-21 13:02:52 +0100

    [Add] UIManager script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	//Name Input Field
+	public InputField nameField;
+
+	//Person's name string
+	private string charName;
+
+	//Your name being displayed at the end
+	public Text yourName;
+
+	// Use this for initialization
+	void Start () {
+		//Personalizes the message at the endscreen
+		yourName.text = "Naughty " + PlayerPrefs.GetString("Your Name");
+	}
+
+	//If you press on the start button
+	public void OnStart(){
+		//Set charName string to text in nameField, saving this in PlayerPrefs
+		if (nameField.text != "") {
+			charName = nameField.text;
+			PlayerPrefs.SetString ("Your Name", charName);
+			Debug.Log("Your name is " + charName);
+		} else {
+			Debug.Log ("You have no name");
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
