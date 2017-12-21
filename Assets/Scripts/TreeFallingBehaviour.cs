ac403be1952fbf9f54c8540db3f83d0add5ed9ed
Arjan <arjan.grinwis@hva.nl>
2017-12-21 14:31:56 +0100

    Update force + sound

--- Assets/Scripts/TreeFallingBehaviour.cs
+++ Assets/Scripts/TreeFallingBehaviour.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class TreeFallingBehaviour : MonoBehaviour {
 
+    public int force;
 	// Use this for initialization
 	void Start () {
-		this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 0));
+		this.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0));
 	}
 	
 	// Update is called once per frame
